e6597e4f8e83f40168d50e332b671da74994aef7
Adam Avenir <adam@avenir.party>
2025-02-13 21:29:29 -0800

    add tsup.config

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'sdk/index.ts',
+    cli: 'cli/index.ts',
+  },
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  splitting: true,
+  treeshake: true,
+  outDir: 'dist',
+  external: [
+    'meld-ast',
+    'meld-spec',
+    'llmxml',
+    'marked',
+    'minimatch',
+    'winston',
+    'yargs'
+  ]
+}); 
