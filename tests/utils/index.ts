6f108652402b39d47f862193a72f44b431f007ce
Adam Avenir <adam@avenir.party>
2025-02-24 23:13:19 -0800

    error handling in test utils

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -4,4 +4,5 @@ export * from './MemfsTestFileSystemAdapter.js';
 export * from './ProjectBuilder.js';
 export * from './TestSnapshot.js';
 export * from './FixtureManager.js';
-export * from './testFactories.js'; 
+export * from './testFactories.js';
+export * from './ErrorTestUtils.js'; 
