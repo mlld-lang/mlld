04ecdbffc78a125f45b6923fffe0853e55d533cc
Adam <adam@andyet.com>
2025-12-16 08:28:32 -0800

    tests

--- grammar/build-grammar.mjs
+++ grammar/build-grammar.mjs
@@ -132,7 +132,9 @@ const allowedStartRules = [
   'WhenConditionList',
   'WhenExpressionConditionList',
   'GuardRuleList',
-  'WhenActionBlockContent'
+  'WhenActionBlockContent',
+  'TemplateBodyAtt',
+  'TemplateBodyMtt'
 ];
 
 const peggyOptsTS = {
