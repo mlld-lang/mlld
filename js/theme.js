5ef50533f15621776ca9894225308b6aac2523a8
adamavenir <adamavenir@users.noreply.github.com>
2026-03-24 05:21:14 +0000

    deploy: 3a9e9bc0a2496f302df8eda3f14a2a307e53b300

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+// This script only adds support for manually toggling themes in the future
+// Current implementation relies on OS preference via CSS media queries
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Theme script loaded - using OS preference');
+});
